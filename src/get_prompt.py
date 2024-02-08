913408b57817e72cd85e207196d345561dc761e1
NolanThompson <j0thom42@louisville.edu>
2024-02-08 11:42:22 -0500

    Saving progress

---
+++ src/get_prompt.py
@@ -0,0 +1,20 @@
+import os
+import openai
+
+def chat_completion(prompt):
+    openai.api_key = os.environ['CHATGPT_KEY']  # ChatGPT auth
+
+    message = [{"role": "system", "content": prompt}]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=message,
+    )
+
+    answer = response.choices[0].message.content
+    return answer
+
+
+def convert(prompt):
+    response = chat_completion(prompt)
+    return response
